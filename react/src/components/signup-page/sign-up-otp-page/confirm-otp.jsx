26f69508554c7d86577b00c0485d9f2408d0048b
munachianya <munachim.anya@pau.edu.ng>
2023-09-15 15:20:27 +0100

    array changed to integer

--- react/src/components/signup-page/sign-up-otp-page/confirm-otp.jsx
+++ react/src/components/signup-page/sign-up-otp-page/confirm-otp.jsx
@@ -3,7 +3,7 @@ import './OtpPage.css';
 import { Link } from 'react-router-dom';
 
 function OTPPage() {
-  const [otp, setOtp] = useState(['', '', '', '', '', '']);
+  const [otp, setOtp] = useState([0, 0, 0, 0, 0, 0]);
   const [timer, setTimer] = useState(60);
   const [isResendDisabled, setIsResendDisabled] = useState(true);
 
@@ -44,13 +44,13 @@ function OTPPage() {
 
   const handleInputChange = (event, index) => {
     const value = event.target.value;
-
-    // Ensure input is numeric and limit length to 1 character
-    if (/^[0-9]*$/.test(value) && value.length <= 1) {
+  
+    // Ensure input is a valid integer (between 0 and 9)
+    if (/^[0-9]$/.test(value)) {
       const updatedOtp = [...otp];
-      updatedOtp[index] = value;
+      updatedOtp[index] = parseInt(value, 10); // Convert the value to an integer
       setOtp(updatedOtp);
-
+  
       // Move focus to the next input field if not the last one
       if (index < 5 && value !== '') {
         const nextInput = document.getElementById(`otp-input-${index + 1}`);
@@ -58,14 +58,30 @@ function OTPPage() {
           nextInput.focus();
         }
       }
+    } else if (value === '' && index > 0) {
+      // If the input is empty and it's not the first one, move focus to the previous input
+      const previousInput = document.getElementById(`otp-input-${index - 1}`);
+      if (previousInput) {
+        previousInput.focus();
+      }
     }
   };
 
   const handleVerifyClick = () => {
-    const enteredOtp = otp.join('');
+     // Combine the OTP array into a single integer
+    const enteredOtp = parseInt(otp.slice(0, 6).join(''), 10);
+     // Check if the entered OTP is a 6-digit integer
+  const isValidOtp = !isNaN(enteredOtp) && enteredOtp >= 100000 && enteredOtp <= 999999;
+  if (isValidOtp) {
     // You can implement your OTP verification logic here
     console.log('Entered OTP:', enteredOtp);
-  };
+
+    // Redirect or perform further actions after successful verification
+  } else {
+    // Display an error message or handle the case where the OTP is invalid
+    console.log('Invalid OTP');
+  }
+};
 
   return (
     <div className='main'>
