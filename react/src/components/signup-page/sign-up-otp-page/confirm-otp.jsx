fdba6c7ed121739cb3919746a1d196a5f4eb6cde
munachianya <munachim.anya@pau.edu.ng>
2023-09-18 09:41:22 +0100

    commit

--- react/src/components/signup-page/sign-up-otp-page/confirm-otp.jsx
+++ react/src/components/signup-page/sign-up-otp-page/confirm-otp.jsx
@@ -3,7 +3,7 @@ import './OtpPage.css';
 import { Link } from 'react-router-dom';
 
 function OTPPage() {
-  const [otp, setOtp] = useState([0, 0, 0, 0, 0, 0]);
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
   const [timer, setTimer] = useState(60);
   const [isResendDisabled, setIsResendDisabled] = useState(true);
 
@@ -12,9 +12,9 @@ function OTPPage() {
     { name: 'Facebook', url: 'https://www.facebook.com' },
     { name: 'Twitter', url: 'https://twitter.com' },
     { name: 'Instagram', url: 'https://www.instagram.com' },
-    
+
   ];
-  
+
   useEffect(() => {
     handleResendClick();
   }, []);
@@ -32,25 +32,25 @@ function OTPPage() {
     return () => {
       clearInterval(timerInterval);
     };
-  },[timer]);
+  }, [timer]);
 
   const handleResendClick = () => {
     // Simulate sending a new OTP here.
     // Reset the timer.
     setTimer(60);
     setIsResendDisabled(true);
-    
+
   };
 
   const handleInputChange = (event, index) => {
     const value = event.target.value;
-  
+
     // Ensure input is a valid integer (between 0 and 9)
     if (/^[0-9]$/.test(value)) {
       const updatedOtp = [...otp];
       updatedOtp[index] = parseInt(value, 10); // Convert the value to an integer
       setOtp(updatedOtp);
-  
+
       // Move focus to the next input field if not the last one
       if (index < 5 && value !== '') {
         const nextInput = document.getElementById(`otp-input-${index + 1}`);
@@ -68,65 +68,65 @@ function OTPPage() {
   };
 
   const handleVerifyClick = () => {
-     // Combine the OTP array into a single integer
+    // Combine the OTP array into a single integer
     const enteredOtp = parseInt(otp.slice(0, 6).join(''), 10);
-     // Check if the entered OTP is a 6-digit integer
-  const isValidOtp = !isNaN(enteredOtp) && enteredOtp >= 100000 && enteredOtp <= 999999;
-  if (isValidOtp) {
-    // You can implement your OTP verification logic here
-    console.log('Entered OTP:', enteredOtp);
+    // Check if the entered OTP is a 6-digit integer
+    const isValidOtp = !isNaN(enteredOtp) && enteredOtp >= 100000 && enteredOtp <= 999999;
+    if (isValidOtp) {
+      // You can implement your OTP verification logic here
+      console.log('Entered OTP:', enteredOtp);
 
-    // Redirect or perform further actions after successful verification
-  } else {
-    // Display an error message or handle the case where the OTP is invalid
-    console.log('Invalid OTP');
-  }
-};
+      // Redirect or perform further actions after successful verification
+    } else {
+      // Display an error message or handle the case where the OTP is invalid
+      console.log('Invalid OTP');
+    }
+  };
 
   return (
     <div className='main'>
-    <div className="otp-container">
-      <h2 style={{fontWeight:"bolder", fontSize:"35px"}}>INPUT OTP</h2>
-      <div className="otp-inputs">
-        {otp.map((value, index) => (
-          <input
-            key={index}
-            id={`otp-input-${index}`}
-            type="text"
-            maxLength={1}
-            value={value}
-            onChange={(event) => handleInputChange(event, index)}
-          />
-        ))}
-      </div>
-      <button onClick={handleVerifyClick} ><Link className='Link' to={"/thank-you-for-signing-up-with-us"}>Verify OTP</Link></button>
-      <div className="timer">{`${Math.floor(timer / 60)}:${(timer % 60)
-        .toString()
-        .padStart(2, '0')}`}</div>
-      <p>
-        Didn't receive the OTP?{' '}
-        <span
-          className={`resend-link ${isResendDisabled ? 'disabled' : ''}`}
-          onClick={isResendDisabled ? undefined : handleResendClick}
-          style={isResendDisabled ? { cursor: 'default' } : { cursor: 'pointer' }}
-        >
-          <p style={{justifyItems:"center", alignItems:"center", paddingLeft:"38px"}}>Resend OTP</p>
-        </span>
-      </p>
-      <div className="social-media">
-        {socialMediaLinks.map((link, index) => (
-          <a
-            key={index}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-media-icon"
+      <div className="otp-container">
+        <h2 style={{ fontWeight: "bolder", fontSize: "35px" }}>INPUT OTP</h2>
+        <div className="otp-inputs">
+          {otp.map((value, index) => (
+            <input
+              key={index}
+              id={`otp-input-${index}`}
+              type="text"
+              maxLength={1}
+              value={value}
+              onChange={(event) => handleInputChange(event, index)}
+            />
+          ))}
+        </div>
+        <button onClick={handleVerifyClick} ><Link className='Link' to={"/thank-you-for-signing-up-with-us"}>Verify OTP</Link></button>
+        <div className="timer">{`${Math.floor(timer / 60)}:${(timer % 60)
+          .toString()
+          .padStart(2, '0')}`}</div>
+        <p>
+          Didn't receive the OTP?{' '}
+          <span
+            className={`resend-link ${isResendDisabled ? 'disabled' : ''}`}
+            onClick={isResendDisabled ? undefined : handleResendClick}
+            style={isResendDisabled ? { cursor: 'default' } : { cursor: 'pointer' }}
           >
-            <i className={`fa fa-${link.name.toLowerCase()}`} />
-          </a>
-        ))}
+            <p style={{ justifyItems: "center", alignItems: "center", paddingLeft: "38px" }}>Resend OTP</p>
+          </span>
+        </p>
+        <div className="social-media">
+          {socialMediaLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-media-icon"
+            >
+              <i className={`fa fa-${link.name.toLowerCase()}`} />
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
