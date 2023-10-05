d4eed5c110aee8986c1a065dde602c63a0da2e30
ise2005best <93008391+ise2005best@users.noreply.github.com>
2023-10-05 09:44:29 +0100

    checkout page started

---
+++ react/src/components/checkout/checkout.component.jsx
@@ -0,0 +1,34 @@
+import React, {useContext} from "react";
+import { CartContext } from "../../context/cart.context";
+import './checkout.styles.scss';
+const CheckOutComponent = () =>{
+    const {cartItems} = useContext(CartContext)
+    return(
+        <div>
+             <h2 className='main-text'>
+                Your shopping cart
+            </h2>
+            <div className='checkout-container'>
+            {
+                cartItems.map((cartItemsId)=>(
+                    <div key={cartItemsId.id} >
+                        <h2 className='products-title'>
+                            {cartItemsId.title}
+                        </h2>
+                        <img src={cartItemsId.imageUrl} alt={`${cartItemsId.title}`}  className='products-image'/>
+                        <h4 className='products-price'>
+                            {cartItemsId.price}
+                        </h4>
+                        <span className='products-quantity'>
+                            {cartItemsId.quantity}
+                        </span>
+                        </div>
+                    
+                ))
+            }
+            </div>
+        </div>
+    )
+}
+
+export default CheckOutComponent;
--- react/src/context/cart.context.js
+++ react/src/context/cart.context.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
       (item) => item.id === productToAdd.id
@@ -17,11 +18,13 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     const existingCartItem = cartItems.find(
       (item) => item.id === cartItemToRemove.id
     );
-  
+      
+    // remove from cart item when quantity is null
     if (existingCartItem.quantity === 1) {
       return cartItems.filter((item) => item.id !== cartItemToRemove.id);
     }
-  
+    
+    // map over new cartItems
     return cartItems.map((item) =>
       item.id === cartItemToRemove.id
         ? { ...item, quantity: item.quantity - 1 }
@@ -44,12 +47,19 @@ export const CartProvider = ({ children }) => {
     const [cartCount, setCartCount] = useState(0)
 
     useEffect(() => {
+      // get total sum of cartItems quantity whenever cartItems changes
         const newCartTotal = cartItems.reduce(
           (total, cartItem) => total + cartItem.quantity,
           0
         );
         setCartCount(newCartTotal);
       }, [cartItems]);
+      
+      var location = useLocation()
+      useEffect(() =>{
+        // close the cart when the user leaves the current page
+        setIsCartOpen(false)
+      },[location.pathname])
 
 
     const addItemsToCart = (productsToAdd) => {
