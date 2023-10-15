a2a77c13ff32318037a51808112f08114f350de5
munachianya <munachim.anya@pau.edu.ng>
2023-10-15 16:09:16 +0100

    checkout-page-ui

--- react/src/components/checkout/checkout.component.jsx
+++ react/src/components/checkout/checkout.component.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useState } from "react";
 import { CartContext } from "../../context/cart.context";
 import { ReactComponent as PLUSICON } from "../../static/noun-plus-1088459.svg";
 import { ReactComponent as MINUSICON } from "../../static/noun-minus-1088464.svg";
+import {ReactComponent as CANCELICON} from "../../static/noun-cancel-1259617.svg";
 import './checkout.styles.scss';
 
 const ProductSection = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, addItemsToCart } = useContext(CartContext);
 
     const calculateTotal = (item) => {
         return item.price * item.quantity;
@@ -13,7 +14,7 @@ const ProductSection = () => {
 
     return (
         <div className="product-section">
-        <h2 className="main-text">Your Shopping Cart</h2>
+        <h2 className="main-texts">Your Shopping Cart</h2>
         <div className="product-list">
             {cartItems.map((cartItem) => (
                 <div className="product-item" key={cartItem.id}>
@@ -23,7 +24,9 @@ const ProductSection = () => {
                             <h3>{cartItem.title}</h3>
                             <p>Price: ₦{cartItem.price}</p>
                             <p>
-                                Quantity: <PLUSICON className="plus-icon" />
+                                
+
+                                Quantity: <PLUSICON className="plus-icon" onClick={console.log("Hello")}/>
                                 {cartItem.quantity}
                                 <MINUSICON className="minus-icon" />
                             </p>
@@ -31,6 +34,7 @@ const ProductSection = () => {
                     </div>
                     <div>
                         <p>₦{calculateTotal(cartItem)}</p>
+                        <CANCELICON className="cancel-icon"/>
                     </div>
                 </div>
             ))}
@@ -52,7 +56,7 @@ const SummarySection = ({ cartTotal }) => {
                 <p>Subtotal: ₦{cartTotal}</p>
                 <p>Shipping: ₦{3000}</p>
                 <p>VAT (7.5%): ₦{0.075*cartTotal}</p>
-                <p>Estimated Total: ₦{}</p>
+                
             </div>
             <div className="discount-section">
                 <input
@@ -63,6 +67,10 @@ const SummarySection = ({ cartTotal }) => {
                 />
                 <button className="discount-button">Apply</button>
             </div>
+            <div className="estimated-total">
+                <p >Estimated Total: ₦{}</p>
+                </div>
+            
             <button className="checkout-button">Checkout</button>
          
         </div>
