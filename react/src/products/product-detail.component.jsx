f31fdbc26736e087a7e5728b9d908db2050d9f62
munachianya <munachim.anya@pau.edu.ng>
2023-10-18 00:29:23 +0100

    new

--- react/src/products/product-detail.component.jsx
+++ react/src/products/product-detail.component.jsx
@@ -1,69 +1,78 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import PRODUCTS from "../products-data.json";
 import "./product-detail.styles.scss";
 
 const ProductDetail = ({ addItemsToCart }) => {
-  const url = window.location.href.split("/");
-  const urlID = url[4];
-  const productId = PRODUCTS[urlID].id - 2;
+    const url = window.location.href.split("/");
+    const urlID = url[4];
+    const productId = PRODUCTS[urlID].id - 2;
 
-  const [quantity, setQuantity] = useState(1);
+    const [quantity, setQuantity] = useState(1);
+    const [mainImage, setMainImage] = useState(PRODUCTS[productId]['tiny-images'][0]);
 
-  const handleQuantityChange = (event) => {
-    setQuantity(parseInt(event.target.value));
-  };
+    const handleQuantityChange = (event) => {
+        setQuantity(parseInt(event.target.value));
+    };
 
-  const handleAddToCart = () => {
-    //add logic to ad to cart
-  };
+    const handleAddToCart = () => {
+        //add logic to ad to cart
+    };
 
-  return (
-    <div className="app">
-      <div className="detail">
-        <div className="big-image">
-          <img
-            src={PRODUCTS[productId].imageUrl}
-            alt={`${PRODUCTS[productId].title}`}
-          />
-        </div> 
-        <div className="box">
-          <div  className="rows">
-            <h2 >{PRODUCTS[productId].title}</h2>
-            <p>{PRODUCTS[productId].price}</p>
-          </div>
-          <form>
-            <label className="quantity-label">Quantity: </label>
-            <select
-              name="quantity"
-              className="quantity-dropdown"
-              value={quantity}
-              onChange={handleQuantityChange}
-            >
-              {[1, 2, 3, 4, 5, 6, 7].map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          </form>
+    const handleImageHover = (newImageUrl) => {
+        setMainImage(newImageUrl);
+      };
 
-          <span className="description">{PRODUCTS[productId].description1}</span>
-          <span className="description">{PRODUCTS[productId].description2}</span>
-          <span className="description">{PRODUCTS[productId].description3}</span>
+    return (
+        <div className="app">
+            <div className="detail">
+                <div className="big-image">
+                    <img
+                        src={mainImage}
+                        alt={`${PRODUCTS[productId].title}`}
+                    />
+                    
+                    <div className="thumb">
+                        {PRODUCTS[productId]["tiny-images"].map((image, index) => (
+                            <img key={index} 
+                            src={image} 
+                            alt={PRODUCTS[productId].title}
+                            onMouseEnter={() => handleImageHover(image)} />
+                        ))}
+                    </div>
 
-          <div className="thumb">
-            {PRODUCTS[productId]["tiny-images"].map((image, index) => (
-              <img key={index} src={image} alt={PRODUCTS[productId].title} />
-            ))}
-          </div>
+                </div>
+                <div className="box">
+                    <div className="rows">
+                        <h2 >{PRODUCTS[productId].title}</h2>
+                        <p>{PRODUCTS[productId].price}</p>
+                    </div>
+                    <form>
+                        <label className="quantity-label">Quantity: </label>
+                        <select
+                            name="quantity"
+                            className="quantity-dropdown"
+                            value={quantity}
+                            onChange={handleQuantityChange}
+                        >
+                            {[1, 2, 3, 4, 5, 6, 7].map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    </form>
 
-          <button className="add-to-cart-button" onClick={handleAddToCart}>
-            Add to Cart
-          </button>
+                    <span className="description">{PRODUCTS[productId].description1} </span>
+                    <span className="description">{PRODUCTS[productId].description2} </span>
+                    <span className="description">{PRODUCTS[productId].description3} </span>
+
+                    <button className="add-to-cart-button" onClick={handleAddToCart}>
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductDetail;
